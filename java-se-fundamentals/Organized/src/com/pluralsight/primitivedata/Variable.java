001443849c56cb5f1c5cee9b05d37ad5e000b172
s0nik <s0nik@debian.dev>
2024-12-15 11:23:43 -0300

    primitive types

---
+++ java-se-fundamentals/Organized/src/com/pluralsight/primitivedata/Variable.java
@@ -0,0 +1,29 @@
+package com.pluralsight.primitivedata;
+
+public class Variable{
+	public static void main(String [] args){
+		
+		final int imutableVariable = 1;	
+		System.out.println("Teste");
+		System.out.println(imutableVariable);
+	
+		System.out.println(imutableVariable);
+	
+	}
+
+
+	private static void integerValues(){
+	
+		byte exByte = 8; // bytes is the most short range storeable in java - 8 bits - min: -128 / max:127 	
+	
+		short exShort = 16; // short is the second minimum range in java - 16 bits - 
+	}
+}	
+
+
+
+
+
+
+
+
