2c13d388f304e70274e53c9d76b3f37ec57dd072
Daniel Ferreira <daniel.ferreira.dev@gmail.com>
2024-12-15 18:25:44 -0300

    integer variables created using vim editor

--- java-se-fundamentals/Organized/src/com/pluralsight/primitivedata/Variable.java
+++ java-se-fundamentals/Organized/src/com/pluralsight/primitivedata/Variable.java
@@ -8,7 +8,7 @@ public class Variable{
 		System.out.println(imutableVariable);
 	
 		System.out.println(imutableVariable);
-	
+        integerValues();	
 	}
 
 
@@ -16,8 +16,17 @@ public class Variable{
 	
 		byte exByte = 8; // bytes is the most short range storeable in java - 8 bits - min: -128 / max:127 	
 	
-		short exShort = 16; // short is the second minimum range in java - 16 bits - 
-	}
+		short exShort = 16; // short is the second minimum range in java - 16 bits -32768 to 32767 
+	    
+        int exInt = 32; // int have thirty two bits 
+    
+        double exDouble = 64; // double have 64 bits and can hold numbers between -2147483648 to 214783647. 
+    
+        System.out.printf("byte is {0} bits and can hold 128 positive/negative numbers\n",exByte);
+        System.out.printf("byte is {0} bits and can hold 32767 positive/negative numbers\n",exShort);
+        System.out.printf("byte is {0} bits and can hold 2147483647 positive/negative numbers\n",exInt);
+        System.out.printf("byte is {0} bits and can hold plus 1 trillions positive/negative numbers\n",exDouble);
+    }
 }	
 
 
